cfacabdf84fdb3edced76e2a6c7473300159140c
Monles <iceueb@gmail.com>
2023-09-13 11:54:28 +0100

    hireme+about

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
-import profilePic from '../../public/images/profile/developer-pic-3.png'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
 import HireMe from '@/components/HireMe'
+import profilePic from '../../public/images/profile/developer-pic-3.png'
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 
 export default function Home() {
   return (
@@ -55,6 +56,9 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightBulb} alt='Yen-Monles' className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
